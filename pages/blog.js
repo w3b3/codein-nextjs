412f79c3900869c4f2f3b91c023c3be878e0603e
Daniel Santos <iam@w-b.dev>
2022-05-31 16:55:52 -0300

    Update mobile experience and entry

--- pages/blog.js
+++ pages/blog.js
@@ -19,17 +19,34 @@ export default function Blog({ data }) {
       </Head>
 
       <main className={styles.main}>
-        <section>
-          <Link href="https://linkedin.ca/in/brasileiro">
-            Hi there! I 🖍 here and on @LinkedIn
-          </Link>
+        <h1>Blog</h1>
+
+        <section className={styles.blogHeader}>
+          <p>
+            The goal is to make this blog a brain dump of relevant notes we have
+            encountered over the course of years engineering web apps.
+          </p>
+          <p>Next steps include:</p>
+          <ol>
+            <li>Add category dropdown to enable post filtering by tags</li>
+            <li>Enable users to sign up (AWS Cognito)</li>
+            <li>Create newsletter flow (AWS SES -> mail list feature)</li>
+            <li>
+              Enable authenticated users to add content (AWS Lambda, API
+              Gateway, DataStax AstraDB - Apache Cassandra DBaaS)
+            </li>
+          </ol>
         </section>
-        <section className={styles.blogContainer}>
+        {/*<Link href="https://linkedin.ca/in/brasileiro">Daniel @LinkedIn</Link>*/}
+        <section className={styles.blogBody}>
+          <h2>Posts</h2>
           {posts.map((el, i) => (
             <article className={styles.post} key={el.post_id}>
               <p className={styles.content}>{el.post_title}</p>
               {el.post_body && <Link href={el.post_body}>{el.post_body}</Link>}
-              <button disabled>Edit</button>
+              {/*<button disabled className={styles.postButton}>*/}
+              {/*  Edit*/}
+              {/*</button>*/}
             </article>
           ))}
         </section>
